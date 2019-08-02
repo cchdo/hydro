66012820c83d38302671621055cd3c1b413081ed
Andrew Barna <abarna@gmail.com>
2019-08-02 16:18:33 -0700

    blackify imported exchange code

--- hydro/core.py
+++ hydro/core.py
@@ -9,7 +9,9 @@ import logging
 log = logging.getLogger(__name__)
 log.setLevel(logging.DEBUG)
 
-fmat = logging.Formatter('%(asctime)s - %(name)s - %(funcName)s - %(levelname)s - %(message)s')
+fmat = logging.Formatter(
+    "%(asctime)s - %(name)s - %(funcName)s - %(levelname)s - %(message)s"
+)
 
 ch = logging.StreamHandler()
 ch.setLevel(logging.DEBUG)
@@ -17,26 +19,9 @@ ch.setFormatter(fmat)
 
 log.addHandler(ch)
 
-WOCECTD_to_ARGO = {
-        1:0,
-        2:1,
-        3:3,
-        4:4,
-        5:9,
-        6:0,
-        7:0,
-        9:9,
-        }
-WOCECTD_to_ODV = {
-        1:0,
-        2:0,
-        3:4,
-        4:8,
-        5:1,
-        6:1,
-        7:1,
-        9:1,
-        }
+WOCECTD_to_ARGO = {1: 0, 2: 1, 3: 3, 4: 4, 5: 9, 6: 0, 7: 0, 9: 9}
+WOCECTD_to_ODV = {1: 0, 2: 0, 3: 4, 4: 8, 5: 1, 6: 1, 7: 1, 9: 1}
+
 
 def _union_checker(union, obj):
     log.debug(f"Checking typing.Union types")
@@ -49,21 +34,25 @@ def _union_checker(union, obj):
     log.debug(f"{type(obj)} not in {union}")
     return False
 
+
 class ValidateInitTypes:
     def __post_init__(self):
         for attr, dtype in self.__annotations__.items():
             log.debug(f"Checking {attr} is {dtype}")
             obj = getattr(self, attr)
             self_type = type(obj)
-            error = (f"Invalid data type for {self!r}: The type of {attr} "
-                    f"is expected to be {dtype} not {self_type}")
-            if 'typing.Union' in str(dtype):
+            error = (
+                f"Invalid data type for {self!r}: The type of {attr} "
+                f"is expected to be {dtype} not {self_type}"
+            )
+            if "typing.Union" in str(dtype):
                 log.debug(f"typing.Union found, checking allowed types")
                 if not _union_checker(dtype, obj):
                     raise TypeError(error)
             elif self_type is not dtype:
                 raise TypeError(error)
         log.debug(f"{self} looks ok")
+
 
 class ToAndFromDict:
     def to_dict(self):
@@ -73,19 +62,22 @@ class ToAndFromDict:
     def from_dict(cls, d):
         return cls(**d)
 
+
 @dataclass(frozen=True)
 class ExchangeCompositeKey(ValidateInitTypes, ToAndFromDict):
     station: str
     cast: int
-    sample: str # may be the pressure value for CTD data
+    sample: str  # may be the pressure value for CTD data
+
 
 @dataclass(frozen=True)
 class ExchangeTimestamp(ValidateInitTypes, ToAndFromDict):
     date: date
     time: Union[time, type(None)]
 
+
 class ExchangeFlagCTD(IntEnum):
-    NOFLAG = 0 # no idea if this will cause issue
+    NOFLAG = 0  # no idea if this will cause issue
     UNCALIBRATED = 1
     GOOD = 2
     QUESTIONABLE = 3
@@ -110,7 +102,7 @@ class ExchangeFlagCTD(IntEnum):
             6: "Interpolated over a pressure interval larger than 2 dbar.",
             7: "Despiked.",
             9: "Not sampled.",
-            }
+        }
         return defs[self.flag]
 
     @property
@@ -119,11 +111,13 @@ class ExchangeFlagCTD(IntEnum):
             return False
         return True
 
+
 class ArgoFlag(IntEnum):
     GOOD = 1
 
+
 class ExchangeFlagSample(IntEnum):
-    NOFLAG = 0 # no idea if this will cause issue
+    NOFLAG = 0  # no idea if this will cause issue
     MISSING = 1
     GOOD = 2
     QUESTIONABLE = 3
@@ -150,15 +144,12 @@ class ExchangeFlagSample(IntEnum):
             7: "Manual chromatographic peak measurement.",
             8: "Irregular digital chromatographic peak integration.",
             9: "Sample not drawn for this measurement from this bottle.",
-            }
+        }
         return defs[self.flag]
 
     def to_other(self, other_flag):
         if other_flag.__name__ == "ArgoFlag":
-            mapping = {
-                    1:1,
-
-                    }
+            mapping = {1: 1}
             # import ArgoFlag
             return None
 
@@ -172,8 +163,9 @@ class ExchangeFlagSample(IntEnum):
             return False
         return True
 
+
 class ExchangeFlagBottle(IntEnum):
-    NOFLAG = 0 # no idea if this will cause issue
+    NOFLAG = 0  # no idea if this will cause issue
     NO_INFO = 1
     GOOD = 2
     LEAKING = 3
@@ -200,7 +192,7 @@ class ExchangeFlagBottle(IntEnum):
             7: "Unknown problem.",
             8: "Pair did not trip correctly. Note that the Niskin bottle can trip at an unplanned depth while the Gerard trips correctly and vice versa.",
             9: "Samples not drawn from this bottle.",
-            }
+        }
         return defs[self.flag]
 
     @property
