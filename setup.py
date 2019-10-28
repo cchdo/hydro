77b0dbe1dd3e1c66f5fe50845b3639f0b0931fe3
Andrew Barna <abarna@gmail.com>
2019-10-28 16:02:52 -0700

    update xarray dep

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     use_scm_version={
-        "write_to": "_version.py",
+        "write_to": "hydro/_version.py",
         "write_to_template": 'version = "{version}"\n',
     }
 )
