2d847ced2a6240e82907cc728f91a54bcd130602
mvkovatch3 <mkovatch@ucsd.edu>
2020-06-04 00:38:08 +0000

    update rename bookkeeping; add xarray intersphinx

--- hydro/rename.py
+++ hydro/rename.py
@@ -14,7 +14,14 @@ def rename_with_bookkeeping(
     name_dict: Optional[Mapping] = None,
     attrs: Optional[List[str]] = None,
 ) -> Dataset:
-    """
+    """Find and update all instances of a given variable to a new name.
+
+    Parameters can be referenced in the attributes of separate parameter (e.g. ``ancillary_variables``) and need to be updated appropriately when renaming variables.
+
+    :param xarray.Dataset xarray_obj: A Dataset containing variables, flags, etc.
+    :param typing.Mapping name_dict: Mapping of old variable names to new.
+    :param typing.List[str] attrs: Names of variable attributes to search through.
+    :rtype: xarray.Dataset
     """
 
     # lets just noop this case
@@ -28,13 +35,15 @@ def rename_with_bookkeeping(
     if attrs is None:
         return renamed
 
+    # find and search through all variables which have attr
     for attr in attrs:
         for var, ds in renamed.filter_by_attrs(**{attr: is_not_none}).items():
-            attr_value = ds.attrs[attr]
+            attr_values = ds.attrs[attr].split(" ")
             for key, value in name_dict.items():
-                attr_value = attr_value.replace(key, value)
-
-            ds.attrs[attr] = attr_value
+                for idx, attr_value in enumerate(attr_values):
+                    if key == attr_value:
+                        attr_values[idx] = attr_value.replace(key, value)
+            renamed[var].attrs[attr] = " ".join(attr_values)
 
     return renamed
 
