a61e73c2b35f85998f752aceaeebedfece370dbd
Andrew Barna <abarna@gmail.com>
2020-09-23 21:45:54 +0000

    Fix exchanges misspell in merge_ex

--- hydro/exchange/merge.py
+++ hydro/exchange/merge.py
@@ -9,22 +9,22 @@ def ordered_merge(*tuples: Tuple[Hashable, ...]):
     return tuple(dict.fromkeys(chain(*tuples)).keys())
 
 
-def merge_ex(*excahnges: Exchange) -> Exchange:
+def merge_ex(*exchanges: Exchange) -> Exchange:
 
-    if len({ex.file_type for ex in excahnges}) != 1:
+    if len({ex.file_type for ex in exchanges}) != 1:
         raise ExchangeInconsistentMergeType
 
-    comments = "\n--FileBreak--\n".join({ex.comments for ex in excahnges})
+    comments = "\n--FileBreak--\n".join({ex.comments for ex in exchanges})
 
-    coordinates2 = dict(coord for ex in excahnges for coord in ex.coordinates.items())
-    data2 = dict(data for ex in excahnges for data in ex.data.items())
+    coordinates2 = dict(coord for ex in exchanges for coord in ex.coordinates.items())
+    data2 = dict(data for ex in exchanges for data in ex.data.items())
     return Exchange(
-        file_type=excahnges[0].file_type,
+        file_type=exchanges[0].file_type,
         comments=comments,
-        parameters=ordered_merge(*(ex.parameters for ex in excahnges)),
-        keys=ordered_merge(*(ex.keys for ex in excahnges)),
-        flags=ordered_merge(*(ex.flags for ex in excahnges)),
-        errors=ordered_merge(*(ex.errors for ex in excahnges)),
+        parameters=ordered_merge(*(ex.parameters for ex in exchanges)),
+        keys=ordered_merge(*(ex.keys for ex in exchanges)),
+        flags=ordered_merge(*(ex.flags for ex in exchanges)),
+        errors=ordered_merge(*(ex.errors for ex in exchanges)),
         coordinates=coordinates2,
         data=data2,
     )
