a1b53bbb5074c4fec2e113086a55f72f83be17e1
Andrew Barna <abrna@gmail.com>
2022-08-15 19:32:04 +0000

    Run black on hydro project

--- cchdo/hydro/tests/conftest.py
+++ cchdo/hydro/tests/conftest.py
@@ -5,13 +5,17 @@ import pytest
 from ..exchange.helpers import simple_bottle_exchange
 from ..exchange import read_exchange
 
+
 @pytest.fixture
 def nc_empty():
     return read_exchange(io.BytesIO(simple_bottle_exchange()))
+
 
 @pytest.fixture
 def nc_placeholder():
     params = ("OXYGEN", "OXYGEN_FLAG_W")
     units = ("UMOL/KG", "")
     data = ("-999", "1")
-    return read_exchange(io.BytesIO(simple_bottle_exchange(params=params, units=units, data=data)))
+    return read_exchange(
+        io.BytesIO(simple_bottle_exchange(params=params, units=units, data=data))
+    )
--- cchdo/hydro/tests/test_merge.py
+++ cchdo/hydro/tests/test_merge.py
@@ -1,14 +1,17 @@
-from .. import accessors
+from .. import accessors  # noqa
+
 
 def test_fq_merge(nc_placeholder):
-    fq = [{
-        "EXPOCODE": "TEST",
-        "STNNBR": "1",
-        "CASTNO": 1,
-        "SAMPNO": "1",
-        "OXYGEN [UMOL/KG]": "234.1",
-        "OXYGEN [UMOL/KG]_FLAG_W": "2"
-    }]
+    fq = [
+        {
+            "EXPOCODE": "TEST",
+            "STNNBR": "1",
+            "CASTNO": 1,
+            "SAMPNO": "1",
+            "OXYGEN [UMOL/KG]": "234.1",
+            "OXYGEN [UMOL/KG]_FLAG_W": "2",
+        }
+    ]
     merged = nc_placeholder.cchdo.merge_fq(fq)
 
     assert merged.oxygen.data == [[234.1]]
