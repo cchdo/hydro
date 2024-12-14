bc808c28eaa2a722f8e7295e53767722bd0fc4ce
Andrew Barna <abarna@gmail.com>
2024-12-14 03:30:30 +0000

    sort out how service worker is loaded

--- docs/conf.py
+++ docs/conf.py
@@ -99,6 +99,7 @@ html_theme = "furo"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+html_extra_path = ["sw.js"]
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
